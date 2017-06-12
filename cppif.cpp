457d1569bfc57dd0d53acd7e519a9f30106c0c01
mnaresh0574 <mnaresh0574@gmail.com>
2017-06-12 12:32:43 -0400

    Created new file

---
+++ cppif.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  int i;
+  cout <<"enter a number"<<endl;
+  cin>>i;
+  if (i==1){
+    cout<<"i am manne"<<endl;
+  }else{
+
+    cout<<"i am NAresh"<<endl;
+  }
+
+}
